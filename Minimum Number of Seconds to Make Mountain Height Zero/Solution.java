45d618c51dc6f4b0a19ba12fd7052d629653095d
Gaurav <gkp5625@gmail.com>
2024-09-22 20:42:05 +0530

    Contest 416

---
+++ Minimum Number of Seconds to Make Mountain Height Zero/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        long low = 0, high = 1;
+        while (!isPossible(high, mountainHeight, workerTimes)) {
+            high *= 2;
+        }
+        while (low < high) {
+            long mid = (low + high) / 2;
+            if (isPossible(mid, mountainHeight, workerTimes)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    boolean isPossible(long maxTime, int mountainHeight, int[] workerTimes) {
+        int totalHeight = 0;
+
+        for (int time : workerTimes) {
+            long maxLevels = (long) ((-1 + Math.sqrt(1 + 8.0 * maxTime / time)) / 2);
+            totalHeight += maxLevels;
+            if (totalHeight >= mountainHeight) {
+                return true;
+            }
+        }
+        return totalHeight >= mountainHeight;
+    }
+}
---
+++ The Two Sneaky Numbers of Digitville/HashMap.java
@@ -0,0 +1,24 @@
+
+public class HashMap<T1, T2> {
+
+    public int getOrDefault(int num, int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOrDefault'");
+    }
+
+    public void put(int num, int i) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'put'");
+    }
+
+    public int[] keySet() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keySet'");
+    }
+
+    public int get(int num) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+}
