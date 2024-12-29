1a46d37ea64bda0e9dcaae40dc59a16c50b6aaf6
Gaurav <118619820+gp0814@users.noreply.github.com>
2024-12-30 03:43:36 +0530

    Time: 14 ms (79.57%), Space: 43.7 MB (10.87%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,14 +1,46 @@
+// class Solution {
+//     public int threeSumClosest(int[] nums, int target) {
+//         int closest = nums[0] + nums[1] + nums[2];
+
+//         for (int i = 0; i < nums.length - 2; i++) {
+//             for (int j = i + 1; j < nums.length - 1; j++) {
+//                 for (int k = j + 1; k < nums.length; k++) {
+//                     int currentSum = nums[i] + nums[j] + nums[k];
+//                     if (Math.abs(target - currentSum) < Math.abs(target - closest)) {
+//                         closest = currentSum;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return closest;
+//     }
+// }
+import java.util.Arrays;
+
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
         int closest = nums[0] + nums[1] + nums[2];
 
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int currentSum = nums[i] + nums[j] + nums[k];
-                    if (Math.abs(target - currentSum) < Math.abs(target - closest)) {
-                        closest = currentSum;
-                    }
+            int left = i + 1, right = nums.length - 1;
+
+            while (left < right) {
+                int currSum = nums[i] + nums[left] + nums[right];
+
+                if (currSum == target) {
+                    return target;
+                }
+
+                if (Math.abs(currSum - target) < Math.abs(closest - target)) {
+                    closest = currSum;
+                }
+
+                if (currSum < target) {
+                    left++;
+                } else {
+                    right--;
                 }
             }
         }
