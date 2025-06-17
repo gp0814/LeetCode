c1272b026d8db7b654428191913eaf2e0f29a26d
Gaurav <118619820+gp0814@users.noreply.github.com>
2025-06-17 10:55:55 +0530

    Time: 18 ms (61.85%), Space: 43.6 MB (98.42%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,47 +1,47 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        if (nums == null || nums.length < 4) {
-            return result;
-        }
-
+        int n = nums.length;
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue; // Skip duplicate
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
             }
-
-            for (int j = i + 1; j < nums.length - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue; // Skip duplicate
+            for(int j=i+1;j<n;j++){
+                if(j>i+1&&nums[j]== nums[j-1]){
+                    continue;
                 }
-
-                int low = j + 1;
-                int high = nums.length - 1;
-
-                while (low < high) {
-                    long sum = (long) nums[i] + nums[j] + nums[low] + nums[high];
-                    if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[low], nums[high]));
-
-                        // Move pointers and skip duplicates
-                        while (low < high && nums[low] == nums[low + 1]) {
-                            low++;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long sum =(long) nums[i]+nums[j]+nums[k]+nums[l];
+                    if(sum == target){
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[k]);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]){
+                            k++;
                         }
-                        while (low < high && nums[high] == nums[high - 1]) {
-                            high--;
+                        while(k<l && nums[l]==nums[l+1]){
+                            l--;
                         }
-                        low++;
-                        high--;
-                    } else if (sum < target) {
-                        low++;
-                    } else {
-                        high--;
+                    }
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        l--;
                     }
                 }
             }
         }
-        return result;
+        return ans;
+        
     }
 }
